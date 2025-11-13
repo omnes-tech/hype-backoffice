d407c5ddf2476d3df18e66bf8e1d294a633e035c
Junio Leonel <junioleonelaraujo@gmail.com>
2025-11-13 19:07:21 -0300

    feat: add HypeApp logo to public layout for branding enhancement

--- src/screens/(public)/_layout.tsx
+++ src/screens/(public)/_layout.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import { Outlet } from "@tanstack/react-router";
 
 import authLayoutImage from "@/assets/images/auth-layout-image.png";
+import hypeappLogo from "@/assets/images/hypeapp-logo.png";
 
 export const Route = createFileRoute("/(public)")({
   component: RouteComponent,
@@ -10,10 +11,16 @@ export const Route = createFileRoute("/(public)")({
 function RouteComponent() {
   return (
     <div className="w-full h-screen bg-neutral-050 flex">
-      <div className="w-1/2">
+      <div className="w-1/2 relative">
+        <img
+          src={hypeappLogo}
+          alt="HypeApp Logo"
+          className="absolute top-10 left-8"
+        />
+
         <img
           src={authLayoutImage}
-          alt="HypeApp"
+          alt="HypeApp Auth Layout Image"
           className="w-full h-full object-cover"
         />
       </div>
